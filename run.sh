b8cc80ded3465e55b75046ff0857f6772fab173e
Dan Munro <dan@danmunro.com>
2025-03-22 15:36:33 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -5,10 +5,6 @@
 
 export IMAGE=$1
 docker run --publish 4000:4000 \
-           --mount type=bind,src=$PWD/clan,dst=/mud/moosehead/clan \
-           --mount type=bind,src=$PWD/gods,dst=/mud/moosehead/gods \
-           --mount type=bind,src=$PWD/jail,dst=/mud/moosehead/jail \
-           --mount type=bind,src=$PWD/jerks,dst=/mud/moosehead/jerks \
            --mount type=volume,src=mud-data,dst=/mud/moosehead/data \
            --restart unless-stopped \
            --detach \
