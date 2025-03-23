8105de02be8c57572848c8b5d40e9045d894f50b
Dan Munro <dan@danmunro.com>
2025-03-23 11:58:54 -0700

    add a test for the buffer overflow fix

--- test/main.c
+++ test/main.c
@@ -1,7 +1,9 @@
 #include <tap.h>
 #include "../src/merc.h"
 #include "handler_test.c"
+#include "skills_test.c"
 
 int main(int argc, char **argv) {
     run_handler_tests();
+    run_skills_tests();
 }
---
+++ test/skills_test.c
@@ -0,0 +1,16 @@
+void test_skill_level_only_reclass_has_skill() {
+    // given
+    CHAR_DATA ch;
+
+    // when
+    ch.race = race_lookup("human");
+    ch.class = class_lookup("samurai");
+    int sn = skill_lookup("chee");
+
+    // then - the skill level should be determined by the primary class
+    ok(skill_table[sn].skill_level[ch.class] == skill_level(&ch, sn));
+}
+
+void run_skills_tests() {
+    test_skill_level_only_reclass_has_skill();
+}
