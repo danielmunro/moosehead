5d126b1137a5a0364d72e7f6ffacbd020e585e32
Dan Munro <dan@danmunro.com>
2025-03-23 13:36:09 -0700

    remove possibly uninitialized warning for port

--- src/main.c
+++ src/main.c
@@ -3,17 +3,12 @@
 #include "merc.h"
 
 int main( int argc, char **argv ) {
-    int port;
-
-    if (argc > 1) {
-        if (!is_number(argv[1])) {
-            char log_buf[MAX_STRING_LENGTH];
-            sprintf(log_buf, "Usage: %s [port #]\n", argv[0]);
-            log_error(log_buf);
-            exit(1);
-        }
-        port = atoi(argv[1]);
+    if (argc != 2 || !is_number(argv[1])) {
+        char log_buf[MAX_STRING_LENGTH];
+        sprintf(log_buf, "Usage: %s [port #]\n", argv[0]);
+        log_error(log_buf);
+        exit(1);
     }
 
-    return run(port);
+    return run(atoi(argv[1]));
 }
