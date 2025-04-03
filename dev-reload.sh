cf17f4cd7c68d438057f762ccf3af9fe6f319dbb
Dan Munro <dan@danmunro.com>
2025-04-02 20:33:38 -0700

    dev reload script

---
+++ dev-reload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -o xtrace
+
+CONTAINER=$(docker ps --quiet --filter label=mhs)
+
+if [ ! -z "$CONTAINER" ]; then
+  docker stop $CONTAINER
+fi
+docker build --progress=plain -t mhs:latest . &> build.log
+./run.sh mhs latest
