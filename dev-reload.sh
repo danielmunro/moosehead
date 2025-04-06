1e22d7e725461d47822afb15c3b251a9c5a2f507
Dan Munro <dan@danmunro.com>
2025-04-06 08:21:48 -0700

    clean up the dev-reload.sh script: * build the new container first before stopping the old one * remove the old one after stopping it * reuse the CONTAINER variable

--- dev-reload.sh
+++ dev-reload.sh
@@ -3,14 +3,15 @@
 set -e
 set -o xtrace
 
+docker build -t mhs:latest .
+
 CONTAINER=$(docker ps --quiet --filter label=mhs)
 
 if [ ! -z "$CONTAINER" ]; then
   docker stop $CONTAINER
+  docker rm $CONTAINER
 fi
 
-docker build -t mhs:latest .
+CONTAINER=$(./run.sh mhs latest)
 
-RUNNING_CONTAINER=$(./run.sh mhs latest)
-
-docker logs -f $RUNNING_CONTAINER
+docker logs -f $CONTAINER
