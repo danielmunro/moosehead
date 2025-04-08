0a983a2eefcb8cca6d48e5171dea7c8ee34937ef
Dan Munro <dan@danmunro.com>
2025-04-08 07:34:09 -0700

    add debug logging, log levels

--- src/log.c
+++ src/log.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include "log.h"
+
+void log_debug(const char *str) {
+    if (LOG_LEVEL >= LOG_DEBUG) {
+        printf("[debug] %s\n", str);
+    }
+}
 
 /*
  * Writes a string to stdout.  No specific log level.
  */
 void log_string(const char *str) {
-    printf("%s\n", str);
+    if (LOG_LEVEL >= LOG_INFO) {
+        printf("[info ] %s\n", str);
+    }
 }
 
 /*
  * Writes a string to stderr.
  */
 void log_error(const char *str) {
-    fprintf(stderr, "%s\n", str);
+    fprintf(stderr, "[error] %s\n", str);
 }
--- src/log.h
+++ src/log.h
@@ -1,7 +1,18 @@
 #ifndef MOOSEHEAD_LOG_H
 #define MOOSEHEAD_LOG_H
 
+void log_debug(const char *str);
 void log_string(const char *str);
 void log_error(const char *str);
+
+enum LogLevel {
+    LOG_ERROR,
+    LOG_INFO,
+    LOG_DEBUG
+};
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_INFO
+#endif
 
 #endif //MOOSEHEAD_LOG_H
