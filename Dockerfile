00bf621b718050aa050830500866a38ca1b60b0a
Dan Munro <dan@danmunro.com>
2025-04-15 07:29:37 -0700

    initial commit of help endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM ubuntu:24.04
 
 RUN apt update
-RUN apt install -y -- curl gcc libgc-dev libatomic-ops-dev libjansson-dev make file gdb
+RUN apt install -y -- curl gcc libgc-dev libatomic-ops-dev libjansson-dev make file gdb git
+
+RUN git clone https://github.com/jacketizer/libyuarel.git
+
+WORKDIR /libyuarel
+RUN make
+RUN make install
 
 WORKDIR /mud/moosehead
 COPY src src
--- src/http.c
+++ src/http.c
@@ -10,6 +10,7 @@
 #include <gc.h>
 #include <jansson.h>
 #include <regex.h>
+#include <yuarel.h>
 
 #include "merc.h"
 #include "log.h"
@@ -19,24 +20,26 @@ int server_fd;
 struct sockaddr_in server_addr;
 const int BUFFER_SIZE = 104857600;
 
-char *index_endpoint();
-char *players_endpoint();
-char *races_endpoint();
-char *build_endpoint();
-char *classes_endpoint();
+char *index_endpoint(struct yuarel url);
+char *players_endpoint(struct yuarel url);
+char *races_endpoint(struct yuarel url);
+char *build_endpoint(struct yuarel url);
+char *classes_endpoint(struct yuarel url);
+char *help_endpoint(struct yuarel url);
 
 typedef struct {
     char *method;
     char *path;
-    char *(*response)();
+    char *(*response)(struct yuarel url);
 } Endpoint;
 
 Endpoint endpoints[] = {
-        {"GET", "/", index_endpoint},
-        {"GET", "/players", players_endpoint},
-        {"GET", "/races", races_endpoint},
-        {"GET", "/build", build_endpoint},
-        {"GET", "/classes", classes_endpoint}
+        {"GET", "", index_endpoint},
+        {"GET", "players", players_endpoint},
+        {"GET", "races", races_endpoint},
+        {"GET", "build", build_endpoint},
+        {"GET", "classes", classes_endpoint},
+        {"GET", "help", help_endpoint}
 };
 
 void init_http_socket(const int port) {
@@ -85,11 +88,35 @@ void build_response(
     *response_len = strlen(response);
 }
 
-char *index_endpoint() {
+char *index_endpoint(struct yuarel url) {
     return "";
 }
 
-char *classes_endpoint() {
+char *help_endpoint(struct yuarel url) {
+    struct yuarel_param params[1];
+
+    if (yuarel_parse_query(url.query, '&', params, 1) < 0) {
+        log_info("unable to parse query");
+        return "";
+    }
+
+    if (strcmp(params[0].key, "query") != 0) {
+        log_info("got unexpected query parameter");
+        return "Url parameter must be `query`";
+    }
+
+    json_auto_t *response = json_object();
+    for (HELP_TRACKER *pTrack = help_tracks[0]; pTrack != NULL; pTrack = pTrack->next) {
+        if (str_cmp(params[0].val, pTrack->keyword) == 0) {
+            json_auto_t *help_text = json_string(pTrack->help->text);
+            json_object_set(response, "help", help_text);
+            break;
+        }
+    }
+    return json_dumps(response, JSON_INDENT(4));
+}
+
+char *classes_endpoint(struct yuarel url) {
     json_auto_t *classes = json_array();
     for (int i = 0; strcmp(class_table[i].name, "null") != 0; i++) {
         json_auto_t *class = json_object();
@@ -113,7 +140,7 @@ char *classes_endpoint() {
     return json_dumps(classes, JSON_INDENT(4));
 }
 
-char *players_endpoint() {
+char *players_endpoint(struct yuarel url) {
     json_auto_t *all_players = json_array();
     DESCRIPTOR_DATA *d;
     CHAR_DATA *wch;
@@ -153,14 +180,14 @@ char *players_endpoint() {
     return json_dumps(all_players, JSON_INDENT(4));
 }
 
-char *build_endpoint() {
+char *build_endpoint(struct yuarel url) {
     json_auto_t *resp = json_object();
     json_auto_t *build = json_string(build_version);
     json_object_set(resp, "build", build);
     return json_dumps(resp, JSON_INDENT(4));
 }
 
-char *races_endpoint() {
+char *races_endpoint(struct yuarel url) {
     json_auto_t *resp = json_array();
     for (int i = 0; pc_race_table[i].name != NULL; i++) {
         json_auto_t *race = json_object();
@@ -205,8 +232,8 @@ char *races_endpoint() {
 
 void handle_client(int client_fd) {
     char *buffer = (char *) malloc(BUFFER_SIZE * sizeof(char));
-
     ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
+
     if (bytes_received > 0) {
         char *response = (char *) malloc(BUFFER_SIZE * 2 * sizeof(char));
         size_t response_len;
@@ -223,27 +250,32 @@ void handle_client(int client_fd) {
             char path[path_len + 1];
             strncpy(path, &buffer[matches[3].rm_so], path_len);
             path[path_len] = '\0';
-
-            int endpoint_count = sizeof(endpoints) / sizeof(endpoints[0]);
-            bool found = false;
-            for (int i = 0; i < endpoint_count; i++) {
-                if (strcmp(method, endpoints[i].method) == 0
-                    && strcmp(path, endpoints[i].path) == 0) {
-                    output = endpoints[i].response();
-                    found = true;
-                } else if (strcmp(method, "HEAD") == 0
-                            && strcmp(path, endpoints[i].path) == 0) {
-                    found = true;
-                }
-            }
-
-            if (found) {
-                build_response("200 OK", output, response, &response_len);
+            struct yuarel url;
+            if (yuarel_parse(&url, path) < 0) {
+                sprintf(log_buf, "invalid url requested :: %s", path);
+                log_info(log_buf);
             } else {
-                build_response("404 Not Found", "", response, &response_len);
-            }
+                int endpoint_count = sizeof(endpoints) / sizeof(endpoints[0]);
+                bool found = false;
+                for (int i = 0; i < endpoint_count; i++) {
+                    if (strcmp(method, endpoints[i].method) == 0
+                        && strcmp(url.path, endpoints[i].path) == 0) {
+                        output = endpoints[i].response(url);
+                        found = true;
+                    } else if (strcmp(method, "HEAD") == 0
+                               && strcmp(url.path, endpoints[i].path) == 0) {
+                        found = true;
+                    }
+                }
 
-            send(client_fd, response, response_len, 0);
+                if (found) {
+                    build_response("200 OK", output, response, &response_len);
+                } else {
+                    build_response("404 Not Found", "", response, &response_len);
+                }
+
+                send(client_fd, response, response_len, 0);
+            }
         }
 
         free(response);
