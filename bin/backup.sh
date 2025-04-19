f874539107eae0f25f178124dcaddec19a9d2767
Dan Munro <dan@danmunro.com>
2025-04-19 08:40:55 -0700

    One cli to rule them all

--- bin/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ENV_FILE=$1
-
-docker run --volume mud-data:/backup/data \
-           --env-file $ENV_FILE \
-           --rm \
-           --entrypoint backup \
-           offen/docker-volume-backup:v2
