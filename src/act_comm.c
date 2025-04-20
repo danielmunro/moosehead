f4039ab1c84c1533acc93971e21854710715a6ae
Dan Munro <dan@danmunro.com>
2025-04-20 12:37:27 -0700

    more clean up

--- src/act_comm.c
+++ src/act_comm.c
@@ -25,14 +25,12 @@
 #include "merc.h"
 #include "act_info.h"
 #include "act_wiz.h"
+#include "comm.h"
 #include "recycle.h"
 #include "tables.h"
 #include "interp.h"
 #include "lookup.h"
 #include "gladiator.h"
-
-/* In comm.c */
-void count_clanners(void);
 
 /* Locals */
 void reclass (CHAR_DATA *ch, int class, bool fPenalty);
--- src/comm.h
+++ src/comm.h
@@ -6,5 +6,6 @@ extern const char *build_version;
 int run(const int mud_port, const int http_port);
 bool write_to_descriptor(int desc, char *str, int length, DESCRIPTOR_DATA *d);
 bool check_parse_name (char *name);
+void count_clanners (void);
 
 #endif //MOOSEHEAD_COMM_H
